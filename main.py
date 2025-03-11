7074989990008d07e914a9b934eb3147db64355a
kanyebek <koshonovkanybek@gmail.com>
2025-03-11 19:54:29 +0600

    ignore

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import flet as ft
 
 def main(page: ft.Page):
     page.title = 'My first app'
-
+    page.theme_mode = ft.ThemeMode.LIGHT
     greeting_text = ft.Text('Hello, world!')
 
     name_input = ft.TextField(label='Name:', autofocus=True)
@@ -18,9 +18,21 @@ def main(page: ft.Page):
             greeting_text.value = 'Write your name plz'
 
         page.update()
+    
+    def toggle_theme(e):
+        if page.theme_mode == ft.ThemeMode.LIGHT:
+            page.theme_mode = ft.ThemeMode.DARK
+        else:
+            page.theme_mode = ft.ThemeMode.LIGHT
+        page.update()
+
+    theme_button = ft.IconButton(icon=ft.icons.BRIGHTNESS_6, tooltip="Сменить тему", on_click=toggle_theme)
 
     greet_button = ft.ElevatedButton('Greet', on_click=on_button_click)
 
-    page.add(greeting_text, name_input, greet_button)
+    page.add(greeting_textft.Row([theme_button], 
+            name_input,
+            greet_button)
+             )
 ft.app(target=main)
 # ft.app(target=main, view=ft.WEB_BROWSER)
